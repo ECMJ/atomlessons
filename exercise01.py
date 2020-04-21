9fc6806cacfae9f668332d0146f6805e951b1114
Evandro Mourão <evandromouraojr@users.noreply.github.com>
2020-04-21 11:35:45 -0300

    learning atom

---
+++ exercise01.py
@@ -0,0 +1,18 @@
+num1 = 45
+num2 = 32
+sum = num1 + num2
+print(sum)
+teste = float(input(""))
+if teste == 0:
+    print("Lucky boy!")
+elif teste == 1:
+    print("Try again!")
+elif teste == 2:
+    print("You're cold!")
+else:
+    print("Go back to sleep!")
+for i, j in enumerate(0, 10):
+    if i > j:
+        print("Ok")
+    else:
+        print(teste)
